50dafcaa720c6756aaf287aa3f0391343988adae
Elias Vasquez <evasq710@hotmail.com>
2024-09-04 01:51:52 -0600

    Establishing Arduino connection

--- Fase 2/API_ARDUINO/src/app.js
+++ Fase 2/API_ARDUINO/src/app.js
@@ -1,8 +1,7 @@
 import express from 'express';
 import { arduinoConnection } from './arduino/connection.js';
 import { getSensorDocs } from './firebase/getDocs.js';
-
-import { addSensorValue } from '../firebase/insertDoc.js';
+import { addSensorValue } from './firebase/insertDoc.js';
 
 const app = express();
 
@@ -23,48 +22,51 @@ app.listen(app.get('port'), () => {
 })
 
 /**
- * Getting all documentos from Firebase DB
+ * Getting all documents from Firebase DB
  */
 const getAllDocs = async () => {
     const documents = await getSensorDocs();
+    console.log("== DOCUMENTS FROM FIREBASE DB ==")
     documents.forEach(doc => {
-        console.log(`${doc.id} => ${doc.data()}`);
+        console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
     })
 }
 
 /**
- * 
+ * Establishing connection with Arduino
  */
 const setArduinoConnection = async () => {
     const parser = await arduinoConnection('$');
-    
+
     parser.on('open', () => {
         console.log('Opened Serial Port.');
     })
-
-    parser.on('data', (arduinoData) => {
+ 
+    parser.on('data', async (arduinoData) => {
         // Si viene '#' al inicio, son Serial.println's basura
-        if (arduinoData[0] == '#') {
+        if (arduinoData.toString().charAt(0) == '#') {
             return;
         }
         
-        console.log("=== Datos recibidos de Arduino ===");
-        console.log(arduinoData);
+        console.log("=== Datos de SENSORES - ARDUINO ===");
+        console.log(arduinoData.toString());
 
-        var sensorValues = arduinoData.split('\n');
-        sensorValues.forEach(sensorValue => {
+        var sensorValues = arduinoData.toString().split('\n');
+
+        for(const sensorValue of sensorValues){
             var sensorData = sensorValue.split('/');
             if (sensorData.length != 2 ||
                 sensorData[0] != "HUMEDAD" && sensorData[0] != "CO2" && sensorData[0] !=  "TEMPERATURA" && sensorData[0] != "DISTANCIA" && sensorData[0] != "LUZ") {
-              return;
+              continue;
             }
             
-            addNewSensorValue(sensorData);
-        })
+            await addNewSensorValue(sensorData);
+        }
     })
 }
 
-const addNewSensorValue = async () => {
+// Adding a new document to the Firebase DB
+const addNewSensorValue = async (sensorData) => {
     try {
         const response = await addSensorValue(sensorData);
         console.log(`New DB entry. ID: ${response.id}`);
--- Fase 2/API_ARDUINO/src/arduino/connection.js
+++ Fase 2/API_ARDUINO/src/arduino/connection.js
@@ -4,22 +4,44 @@ import { setTimeout } from 'timers/promises';
 
 
 export const arduinoConnection = async (delimiter) => {
-    while (true) {
-        try {
-            var comPort = process.env.COM_PORT || "COM3";
-            console.log(`Trying serial connection through ${comPort}.`);
+    var tryConnection = true;
+    var comPort = process.env.COM_PORT || "COM3";
 
-            const port = new SerialPort({
-                path: comPort,
+    while (tryConnection) {
+        try {
+            tryConnection = false;
+            process.stdout.write(`Trying serial connection through ${comPort}... `);
+
+            var port = new SerialPort({
+                path: comPort, 
                 baudRate: 9600
             });
+            port.on('error', () => {
+                tryConnection = true;
+            })
+
+            // Giving time to connect
+            await setTimeout(2000);
+
+            if (tryConnection){
+                // The connection with Arduino failed, trying again
+                console.log("Failed, waiting 3s.");
+                await setTimeout(1000);
+                continue;
+            }
+
+            // If the prograam execution reaches this code, the connection with Arduino was established
+            console.log("Done!");
+            await setTimeout(1000);
+
             const parser = port.pipe(new DelimiterParser({
                 delimiter: delimiter
             }))
+
             return parser;
+            
         } catch (error) {
-            console.log("Connection failed, waiting 3s...");
-            await setTimeout(3000);
+            throw new Error(error);
         }
     }
 };
