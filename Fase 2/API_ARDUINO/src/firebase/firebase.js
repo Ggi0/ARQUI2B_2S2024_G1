023fadbc861d934684d15ace36e2f2e80c0316bb
Elias Vasquez <evasq710@hotmail.com>
2024-09-07 11:21:04 -0600

    New firebase configuration

--- Fase 2/API_ARDUINO/src/firebase/firebase.js
+++ Fase 2/API_ARDUINO/src/firebase/firebase.js
@@ -4,9 +4,9 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
-    authDomain: "arqui2-db.firebaseapp.com",
-    projectId: "arqui2-db",
-    storageBucket: "arqui2-db.appspot.com",
+    authDomain: "arqui2-bd-backup.firebaseapp.com",
+    projectId: "arqui2-bd-backup",
+    storageBucket: "arqui2-bd-backup.appspot.com",
     messagingSenderId: process.env.MESSAGING_SENDER_ID,
     appId: process.env.APP_ID
 };
